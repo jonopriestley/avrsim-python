5988f18254fe79d35f74b359a1aa0725365dd698
jonopriestley <99158173+jonopriestley@users.noreply.github.com>
2022-03-29 16:03:11 +1100

    Add files via upload
    Added all files

---
+++ AVR Interpreter/AVR Sim/sim/avr_error.py
@@ -0,0 +1,33 @@
+######################################
+#  ERRORS
+######################################
+
+class Error:
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        result += f'\nFile {self.pos_start.fn}, line {self.pos_start.ln}'
+        return result
+
+    def __repr__(self):
+        result = f'{self.error_name}: {self.details}'
+        result += f'\nFile {self.pos_start.fn}, line {self.pos_start.ln}'
+        return result
+
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+class InvalidInstructionError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Invalid Instruction', details)
+
+class UnexpectedValue(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Unexpected Value', details)
+
